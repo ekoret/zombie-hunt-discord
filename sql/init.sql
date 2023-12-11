ee6019c966973e851d60992fcf286fc741a68111
ekoret <ekodevelops@gmail.com>
2023-12-11 13:02:17 -0800

    update sql

--- sql/init.sql
+++ sql/init.sql
@@ -3,34 +3,35 @@ CREATE DATABASE IF NOT EXISTS DevDatabase;
 USE DevDatabase;
 
 CREATE TABLE IF NOT EXISTS users (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id BIGINT NOT NULL,
     username VARCHAR(255) NOT NULL,
     nickname VARCHAR(255) DEFAULT NULL,
     email VARCHAR(255) DEFAULT NULL
 );
 
 CREATE TABLE IF NOT EXISTS games (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL UNIQUE,
     description TEXT
 );
 
 CREATE TABLE IF NOT EXISTS zombiehunt_stats (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    game_id INTEGER NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    game_id INT NOT NULL,
     user_id BIGINT NOT NULL,
-    total_kills INTEGER DEFAULT 0,
-    ammo INTEGER DEFAULT 0,
-    meat INTEGER DEFAULT 0,
+    total_kills INT DEFAULT 0,
+    ammo INT DEFAULT 0,
+    meat INT DEFAULT 0,
     FOREIGN KEY (game_id) REFERENCES games(id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
 CREATE TABLE IF NOT EXISTS daily_claims (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     user_id BIGINT NOT NULL,
     claim_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
 INSERT IGNORE INTO games (name, description) VALUES ('Zombie Hunt', 'Shoot zombies for meat! Trade meat for points or more gear.');
